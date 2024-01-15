dced86f534a11ffa16c3635e61d097dd56737b21
kuqmua <serezhashmakov@outlook.com>
2024-01-15 10:49:20 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,51 @@
-#[proc_macro_derive(GenerateToCamelCaseEnumUnnamedVariants)]//todo check on postgresql max length value of type
-pub fn generate_to_camel_case_enum_unnamed_variants(input: proc_macro::TokenStream) -> proc_macro::TokenStream {//todo in few cases rows affected is usefull. (update delete for example). if 0 afftected -maybe its error? or maybe use select then update\delete?(rewrite query)
+///only works if all enum variants without fields like this
+/// #[derive(proc_macro_assistants::GenerateToUpperCamelCase)]
+/// enum Operation {
+///     One,
+///     Two,
+///     Three,
+/// }
+#[proc_macro_derive(GenerateToUpperCamelCase)]
+pub fn generate_to_upper_camel_case(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro_helpers::panic_location::panic_location();
-    let proc_macro_name_camel_case = "GenerateToCamelCaseEnumUnnamedVariants";
-    // let proc_macro_name_lower_case = proc_macro_helpers::to_lower_snake_case::ToLowerSnakeCase::to_lower_snake_case(&proc_macro_name_camel_case);
+    let proc_macro_name_camel_case = "GenerateToUpperCamelCase";
     let ast: syn::DeriveInput = syn::parse(input).unwrap_or_else(|e| {
         panic!(
             "{proc_macro_name_camel_case} {}: {e}",
             proc_macro_helpers::global_variables::hardcode::AST_PARSE_FAILED
         )
     });
-    // println!("{:#?}", ast.data);
     let ident = &ast.ident;
-    // let ident_lower_case_stringified = proc_macro_helpers::to_lower_snake_case::ToLowerSnakeCase::to_lower_snake_case(&ident.to_string());
     let proc_macro_name_camel_case_ident_stringified = format!("{proc_macro_name_camel_case} {ident}");
     let data_enum = if let syn::Data::Enum(data_enum) = &ast.data {
         data_enum
     } else {
         panic!("{proc_macro_name_camel_case_ident_stringified} does work only on structs!");
     };
-    quote::quote! {
-        impl Operation {
-            //todo maybe write simple proc macro for it?
+    let variants_matching_values_token_stream = data_enum
+        .variants
+        .iter()
+        .map(|variant| match &variant.fields {
+            syn::Fields::Unit => {
+                let variant_ident = &variant.ident;
+                let variant_ident_upper_camel_case_stringified = convert_case::Casing::to_case(&variant_ident.to_string(), convert_case::Case::UpperCamel);//todo rename all _camel_case to _upper_camel_case
+                let variant_ident_upper_camel_case_quotes_token_stream = proc_macro_helpers::generate_quotes::generate_quotes_token_stream(
+                    &variant_ident_upper_camel_case_stringified,
+                    &proc_macro_name_camel_case_ident_stringified,
+                );
+                quote::quote! {Self::#variant_ident => #variant_ident_upper_camel_case_quotes_token_stream}
+            },
+            _ => panic!("{proc_macro_name_camel_case} supported only syn::Fields::Unit"),
+        }).collect::<std::vec::Vec<proc_macro2::TokenStream>>();
+    let gen = quote::quote! {
+        impl #ident {
             fn to_camel_case(&self) -> &str {
                 match self {
-                    Self::CreateMany => "CreateMany",
-                    Self::CreateOne => "CreateOne",
-                    Self::ReadMany => "ReadMany",
-                    Self::ReadOne => "ReadOne",
-                    Self::UpdateMany => "UpdateMany",
-                    Self::UpdateOne => "UpdateOne",
-                    Self::DeleteMany => "DeleteMany",
-                    Self::DeleteOne => "DeleteOne",
+                    #(#variants_matching_values_token_stream),*
                 }
             }
         }
-    }.into()
+    };
+    // println!("{gen}");
+    gen.into()
 }
