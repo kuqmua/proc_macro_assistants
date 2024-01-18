d77d0828c8d3e95bd544d60a5e46b554bb1e089f
kuqmua <serezhashmakov@outlook.com>
2024-01-18 23:48:20 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 /*
 only works if all enum variants without fields like this
-#[derive(proc_macro_assistants::ToUpperCamelCase)]
+#[derive(proc_macro_assistants::ToUpperCamelCaseString)]
 enum Operation {
      One,
      Two,
      Three,
 }
 */
-#[proc_macro_derive(ToUpperCamelCase)]
-pub fn to_upper_camel_case(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+#[proc_macro_derive(ToUpperCamelCaseString)]
+pub fn to_upper_camel_case_string(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro_helpers::panic_location::panic_location();
-    let proc_macro_name_upper_camel_case_stringified = "ToUpperCamelCase";
+    let proc_macro_name_upper_camel_case_stringified = "ToUpperCamelCaseString";
     let ast: syn::DeriveInput = syn::parse(input).unwrap_or_else(|e| {
         panic!(
             "{proc_macro_name_upper_camel_case_stringified} {}: {e}",
@@ -28,7 +28,7 @@ pub fn to_upper_camel_case(input: proc_macro::TokenStream) -> proc_macro::TokenS
     let variants_matching_values_token_stream = data_enum.variants.iter().map(|variant| match &variant.fields {
         syn::Fields::Unit => {
             let variant_ident = &variant.ident;
-            let variant_ident_upper_camel_case_stringified = proc_macro_helpers::naming_conventions::ToUpperCamelCase::to_upper_camel_case(&variant_ident.to_string());
+            let variant_ident_upper_camel_case_stringified = proc_macro_helpers::naming_conventions::ToUpperCamelCaseString::to_upper_camel_case_string(&variant_ident.to_string());
             let variant_ident_upper_camel_case_quotes_token_stream = proc_macro_helpers::generate_quotes::generate_quotes_token_stream(
                 &variant_ident_upper_camel_case_stringified,
                 &proc_macro_name_upper_camel_case_ident_stringified,
@@ -61,17 +61,17 @@ pub fn to_upper_camel_case(input: proc_macro::TokenStream) -> proc_macro::TokenS
 
 /* 
 only works if all enum variants without fields like this
- #[derive(proc_macro_assistants::ToSnakeCase)]
+ #[derive(proc_macro_assistants::ToSnakeCaseString)]
  enum Operation {
      One,
      Two,
      Three,
  }
 */
-#[proc_macro_derive(ToSnakeCase)]
-pub fn to_snake_case(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+#[proc_macro_derive(ToSnakeCaseString)]
+pub fn to_snake_case_string(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro_helpers::panic_location::panic_location();
-    let proc_macro_name_upper_camel_case_stringified = "ToSnakeCase";
+    let proc_macro_name_upper_camel_case_stringified = "ToSnakeCaseString";
     let ast: syn::DeriveInput = syn::parse(input).unwrap_or_else(|e| {
         panic!(
             "{proc_macro_name_upper_camel_case_stringified} {}: {e}",
@@ -89,7 +89,7 @@ pub fn to_snake_case(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
     let variants_matching_values_token_stream = data_enum.variants.iter().map(|variant| match &variant.fields {
         syn::Fields::Unit => {
             let variant_ident = &variant.ident;
-            let variant_ident_snake_case_stringified = proc_macro_helpers::naming_conventions::ToSnakeCase::to_snake_case(&variant_ident.to_string());
+            let variant_ident_snake_case_stringified = proc_macro_helpers::naming_conventions::ToSnakeCaseString::to_snake_case_string(&variant_ident.to_string());
             let variant_ident_snake_case_quotes_token_stream = proc_macro_helpers::generate_quotes::generate_quotes_token_stream(
                 &variant_ident_snake_case_stringified,
                 &proc_macro_name_upper_camel_case_ident_stringified,
@@ -121,17 +121,17 @@ pub fn to_snake_case(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
 }
 /* 
 only works if all enum variants without fields like this
- #[derive(proc_macro_assistants::ToScreamingSnakeCase)]
+ #[derive(proc_macro_assistants::ToScreamingSnakeCaseString)]
  enum Operation {
      One,
      Two,
      Three,
  }
 */
-#[proc_macro_derive(ToScreamingSnakeCase)]
-pub fn to_screaming_snake_case(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+#[proc_macro_derive(ToScreamingSnakeCaseString)]
+pub fn to_screaming_snake_case_string(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro_helpers::panic_location::panic_location();
-    let proc_macro_name_upper_camel_case_stringified = "ToScreamingSnakeCase";
+    let proc_macro_name_upper_camel_case_stringified = "ToScreamingSnakeCaseString";
     let ast: syn::DeriveInput = syn::parse(input).unwrap_or_else(|e| {
         panic!(
             "{proc_macro_name_upper_camel_case_stringified} {}: {e}",
@@ -149,7 +149,7 @@ pub fn to_screaming_snake_case(input: proc_macro::TokenStream) -> proc_macro::To
     let variants_matching_values_token_stream = data_enum.variants.iter().map(|variant| match &variant.fields {
         syn::Fields::Unit => {
             let variant_ident = &variant.ident;
-            let variant_ident_snake_case_stringified = proc_macro_helpers::naming_conventions::ToScreamingSnakeCase::to_screaming_snake_case(&variant_ident.to_string());
+            let variant_ident_snake_case_stringified = proc_macro_helpers::naming_conventions::ToScreamingSnakeCaseString::to_screaming_snake_case_string(&variant_ident.to_string());
             let variant_ident_snake_case_quotes_token_stream = proc_macro_helpers::generate_quotes::generate_quotes_token_stream(
                 &variant_ident_snake_case_stringified,
                 &proc_macro_name_upper_camel_case_ident_stringified,
@@ -254,7 +254,7 @@ pub fn payload_upper_camel_case_token_stream(input: proc_macro::TokenStream) -> 
                 let payload_upper_camel_case_stringified = "Payload";
                 let value = format!(
                     "{}{payload_upper_camel_case_stringified}",
-                    proc_macro_helpers::naming_conventions::ToUpperCamelCase::to_upper_camel_case(&variant_ident.to_string())
+                    proc_macro_helpers::naming_conventions::ToUpperCamelCaseString::to_upper_camel_case_string(&variant_ident.to_string())
                 );
                 value.parse::<proc_macro2::TokenStream>()
                 .unwrap_or_else(|_| panic!("{proc_macro_name_upper_camel_case_ident_stringified} {value} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
@@ -306,7 +306,7 @@ pub fn payload_with_serialize_deserialize_upper_camel_case_token_stream(input: p
                 let payload_with_serialize_deserialize_upper_camel_case_stringified = "PayloadWithSerializeDeserialize";
                 let value = format!(
                     "{}{payload_with_serialize_deserialize_upper_camel_case_stringified}",
-                    proc_macro_helpers::naming_conventions::ToUpperCamelCase::to_upper_camel_case(&variant_ident.to_string())
+                    proc_macro_helpers::naming_conventions::ToUpperCamelCaseString::to_upper_camel_case_string(&variant_ident.to_string())
                 );
                 value.parse::<proc_macro2::TokenStream>()
                 .unwrap_or_else(|_| panic!("{proc_macro_name_upper_camel_case_ident_stringified} {value} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
